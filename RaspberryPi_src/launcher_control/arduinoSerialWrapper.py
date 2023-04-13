15270ac8333ba3b5016b6a96616bc69457698830
n00Nspr1ng <seoyeon99@yonsei.ac.kr>
2023-04-13 15:49:19 +0900

    imaginary

--- RaspberryPi_src/launcher_control/arduinoSerialWrapper.py
+++ RaspberryPi_src/launcher_control/arduinoSerialWrapper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import serial
+import time
 
 class arduinoSerialWrapper():
     '''
@@ -10,24 +11,49 @@ class arduinoSerialWrapper():
                 Usually set to /dev/ttyACM0.
         baudrate: Baudrate of arduino. Default is set to 9600.
     '''
-    def __init__(self, device, baudrate=9600):
-        self.arduino = serial.Serial(device, baudrate, timeout=1)
-
-    def resetBuff(self):
-        self.arduino.reset_input_buffer()
     
+    def __init__(self, device, baudrate=9600):
+
+        # Open arduino communication
+        try:
+            self.arduino = serial.Serial(device, baudrate, timeout=1)
+            time.sleep(1)
+        except:
+            print("Device can not be found or can not be configured.")
+
+        self.reset_buff()
+
+
+    def reset_buff(self):
+        self.arduino.reset_input_buffer()
+
+
     def read(self):
         if self.arduino.in_waiting > 0:
             line = self.arduino.reaLine().decode('utf-8').rstrip()
         
         return line
-    #TODO: to be continued
 
-# if __name__ == "__main__":
-#     arduino = serial.Serial(config.ardu_device, 9600, timeout=1)
-#     arduino.reset_input_buffer()
 
-#     while True:
-#         if arduino.in_waiting > 0:
-#             line = arduino.readline().decode('utf-8').rstrip()
-#             print(line)
+    def send_speed(self, motor_speed):
+        self.arduino.write(motor_speed)
+
+        
+    def check_done(self):
+        while(True):
+            if (self.arduino.readable()):
+                done_flag = int(self.arduino.readline().decode('utf-8').rstrip())
+                if (done_flag == True):
+                    break
+        return True
+
+
+if __name__ == "__main__":
+    arduino = arduinoSerialWrapper("/dev/ttyACM0", 9600, timeout=1)
+    arduino.read()
+    time.sleep(1)
+
+    arduino.send_speed(100)
+    arduino.check_done()
+
+    
