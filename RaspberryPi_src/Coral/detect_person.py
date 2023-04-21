e6a442afc7eac43ba92219eb1b8473b74e8cfba2
n00Nspr1ng <seoyeon99@yonsei.ac.kr>
2023-04-21 21:57:04 +0900

    added class version

--- RaspberryPi_src/Coral/detect_person.py
+++ RaspberryPi_src/Coral/detect_person.py
@@ -36,74 +36,99 @@ from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
 from pycoral.utils.edgetpu import run_inference
 
-def main():
-    default_model_dir = '../all_models'
-    default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
-    default_labels = 'coco_labels.txt'
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', help='.tflite model path',
-                        default=os.path.join(default_model_dir,default_model))
-    parser.add_argument('--labels', help='label file path',
-                        default=os.path.join(default_model_dir, default_labels))
-    parser.add_argument('--top_k', type=int, default=3,
-                        help='number of categories with highest score to display')
-    parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default = 0)
-    parser.add_argument('--threshold', type=float, default=0.1,
-                        help='classifier score threshold')
-    args = parser.parse_args()
+class personDetector():
+    def __init__(self, show_image=True):
+        self.show_image = show_image
 
-    print('Loading {} with {} labels.'.format(args.model, args.labels))
-    interpreter = make_interpreter(args.model)
-    interpreter.allocate_tensors()
-    labels = read_label_file(args.labels)
-    inference_size = input_size(interpreter)
+        # Flag for detection start
+        self.is_detected = False 
+        # Initialize bounding box center point
+        self.xc = 0
+        self.yc = 0
 
-    cap = cv2.VideoCapture(args.camera_idx)
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        cv2_im = frame
+    def detect(self):
+        default_model_dir = '../all_models'
+        default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
+        default_labels = 'coco_labels.txt'
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--model', help='.tflite model path',
+                            default=os.path.join(default_model_dir,default_model))
+        parser.add_argument('--labels', help='label file path',
+                            default=os.path.join(default_model_dir, default_labels))
+        parser.add_argument('--top_k', type=int, default=3,
+                            help='number of categories with highest score to display')
+        parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default = 0)
+        parser.add_argument('--threshold', type=float, default=0.1,
+                            help='classifier score threshold')
+        args = parser.parse_args()
 
-        cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
-        cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
-        run_inference(interpreter, cv2_im_rgb.tobytes())
-        objs = get_objects(interpreter, args.threshold)[:args.top_k]
-        cv2_im = append_objs_to_img(cv2_im, inference_size, objs, labels)
+        print('Loading {} with {} labels.'.format(args.model, args.labels))
+        interpreter = make_interpreter(args.model)
+        interpreter.allocate_tensors()
+        labels = read_label_file(args.labels)
+        inference_size = input_size(interpreter)
 
-        cv2.imshow('frame', cv2_im)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        cap = cv2.VideoCapture(args.camera_idx)
 
-    cap.release()
-    cv2.destroyAllWindows()
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            cv2_im = frame
 
-def append_objs_to_img(cv2_im, inference_size, objs, labels):
-    height, width, channels = cv2_im.shape
-    scale_x, scale_y = width / inference_size[0], height / inference_size[1]
-    
-    for obj in objs:
-        
-        if labels[obj.id] == 'person':
-            bbox = obj.bbox.scale(scale_x, scale_y)
-            x0, y0 = int(bbox.xmin), int(bbox.ymin)
-            x1, y1 = int(bbox.xmax), int(bbox.ymax)
-
-            xc = int((x0+x1)/2)
-            yc = int((y0+y1)/2)
-
-            percent = int(100 * obj.score)
-
-            label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
-            cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
-            cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
-                                     cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
-            cv2_im = cv2.circle(cv2_im, (xc,yc),5,(255,255,255),-1)
+            cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
+            cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
+            run_inference(interpreter, cv2_im_rgb.tobytes())
+            objs = get_objects(interpreter, args.threshold)[:args.top_k]
+            cv2_im = self.append_objs_to_img(cv2_im, inference_size, objs, labels)
             
-            print("center coordinates : ", xc,yc)
+            if self.show_image == True:
+                cv2.imshow('frame', cv2_im)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
 
-    return cv2_im
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+    def append_objs_to_img(self, cv2_im, inference_size, objs, labels):
+        height, width, channels = cv2_im.shape
+        scale_x, scale_y = width / inference_size[0], height / inference_size[1]
+            
+        for obj in objs:
+                
+            if labels[obj.id] == 'person':
+                if (self.is_detected == False):
+                    self.is_detected = True
+                bbox = obj.bbox.scale(scale_x, scale_y)
+                x0, y0 = int(bbox.xmin), int(bbox.ymin)
+                x1, y1 = int(bbox.xmax), int(bbox.ymax)
+
+                self.xc = int((x0+x1)/2)
+                self.yc = int((y0+y1)/2)
+                print(self.xc, self.yc)
+
+                percent = int(100 * obj.score)
+
+                label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
+                cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
+                cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
+                                            cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+                cv2_im = cv2.circle(cv2_im, (self.xc,self.yc),5,(255,255,255),-1)
+
+            else:
+                if (self.is_detected == True):
+                    self.is_detected = False
+
+        return cv2_im
+        
+
+    def get_center(self):
+        return self.xc, self.yc
+
 
 if __name__ == '__main__':
-    main()
+    detector = personDetector()
+    detector.detect()
+    print(detector.get_center())
