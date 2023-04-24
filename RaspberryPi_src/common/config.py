d8b11e8f487e49f23a79de8dde5f749c423039da
n00Nspr1ng <seoyeon99@yonsei.ac.kr>
2023-04-24 18:38:16 +0900

    Added servo & step control

--- RaspberryPi_src/common/config.py
+++ RaspberryPi_src/common/config.py
@@ -1,1 +1,13 @@
+from dataclasses import dataclass
+
+# Arduino port number
 ardu_device = "/dev/ttyACM0"
+
+# Step motor for pan, set up as GPIO.BCM
+@dataclass
+class stepmotor:
+    STEP : int = 24
+    DIR : int = 23
+
+# Servo motor for tilt, set up as GPIO.BCM
+servomotor : int = 25
