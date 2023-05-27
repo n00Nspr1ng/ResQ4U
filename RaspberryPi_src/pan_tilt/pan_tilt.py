68228a9d32aa4974fe65e93e6bbe60c16161b012
Dahyun Diana Nam <94898107+dn0908@users.noreply.github.com>
2023-05-27 20:12:29 +0900

    Update pan_tilt.py

--- RaspberryPi_src/pan_tilt/pan_tilt.py
+++ RaspberryPi_src/pan_tilt/pan_tilt.py
@@ -1,17 +1,9 @@
-##########################################################################
-#        READ ME !
-#        CODE BY DAHYUN
-#        DIDNT HAVE TIME TO TEST
-#        CODE WILL BE MODIFIED AS SOON AS MY MID TERM IS OVER !
-#        I WILL MAKE THESE (DETECTION + LIDAR + PAN TILT) CODES TO CLASSES.. 
-#        HAVE A GREAT DAY FOR THOSE WHO ARE READING THIS.......
-
 import cv2
 import RPi.GPIO as GPIO
 
 # set up GPIO pins for servo motors
-panPin = 18 # GPIO pin for pan servo
-tiltPin = 12 # GPIO pin for tilt servo
+panPin   = 18   # GPIO pin for pan servo
+tiltPin  = 12   # GPIO pin for tilt servo
 
 GPIO.setmode(GPIO.BCM) # use BCM numbering scheme
 GPIO.setup(panPin, GPIO.OUT) # set pan pin as output
@@ -25,14 +17,20 @@ tilt = GPIO.PWM(tiltPin, 50) # 50Hz
 pan.start(0)
 tilt.start(0)
 
-# Camera center pixels > (Dahyun will change this code after mid term..)
-center_x = 320
-center_y = 240
+# Camera center point define
+center_x = 320 
+center_y = 240 
 
 # Pan-tilt angles and control step
-pan_angle = 90
-tilt_angle = 90
-step_size = 5
+pan_angle   = 90
+tilt_angle  = 90
+pan_step    = 10
+tilt_step   = 5
+
+pan_angle_min  = 0
+pan_angle_max  = 200
+tilt_angle_min = 0
+tilt_angle_max = 100
 
 # Threshold error - center & max value
 error_threshold = 10
@@ -53,19 +51,19 @@ while True:
 
     if abs(error_x) > error_threshold or abs(error_y) > error_threshold:
         if error_x > 0:
-            pan_angle += step_size
+            pan_angle += pan_step
         if error_x < 0:
-            pan_angle -= step_size
+            pan_angle -= pan_step
 
         if error_y > 0:
-            tilt_angle += step_size
+            tilt_angle += tilt_step
         else: # error_y < 0
-            tilt_angle -= step_size
+            tilt_angle -= tilt_step
         
-        # Pan angle limitation to [50, 130]
-        pan_angle = max(50, min(130, pan_angle))
-        # Limit the tilt angle to [0, 70]
-        tilt_angle = max(0, min(70, tilt_angle))
+        # Pan angle limitation to [pan_angle_min, pan_angle_max]
+        pan_angle = max(pan_angle_min, min(pan_angle_max, pan_angle))
+        # Limit the tilt angle to [tilt_angle_min, tilt_angle_max]
+        tilt_angle = max(tilt_angle_min, min(tilt_angle_max, tilt_angle))
 
 		# move the pan-tilt camera to track the face
 		setAngle(pan_angle, pan)
