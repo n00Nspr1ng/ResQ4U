1738a2a4321cc1de0f2c47985605b5edad14b028
Dahyun Diana Nam <94898107+dn0908@users.noreply.github.com>
2023-05-27 19:52:47 +0900

    0527-1

--- Arduino_src/main/_config.ino
+++ Arduino_src/main/_config.ino
@@ -1,24 +1,24 @@
-//////////// BLDC MOTOR ////////////
+////////////// BLDC MOTOR //////////////
 
-#define    SPEED_IN1   5
-#define    SPEED_IN2   6
-#define         DIR1  10
-#define  START_STOP1  11
-#define         DIR2  12
-#define  START_STOP2  13
+#define    SPEED_IN1   5    // ORANGE  6
+#define    SPEED_IN2   6    // ORANGE  8
+#define         DIR1  10    //  BROWN  2
+#define  START_STOP1  11    //   PINK  4
+#define         DIR2  12    //  BROWN 16
+#define  START_STOP2  13    //   PINK 17
 
-/////////////////////////////////////
+////////////////////////////////////////
 
-//////////// FEEDER STEP ////////////
+////////////// FEEDER STEP //////////////
 
-#define    FEED_STEP  8
-#define     FEED_DIR  9
+#define    FEED_STEP   8
+#define     FEED_DIR   9
 
-/////////////////////////////////////
+/////////////////////////////////////////
 
-/////////////// LiDAR ///////////////
+///////////////// LiDAR /////////////////
 
-#define     LIDAR_RX  2 //green
-#define     LIDAR_TX  3 //white
+#define     LIDAR_RX   2         // GREEN
+#define     LIDAR_TX   3         // WHITE
 
-/////////////////////////////////////
+/////////////////////////////////////////
