dce5e287b9421da8b241afe50cd21fa6a46465bb
Dahyun Diana Nam <94898107+dn0908@users.noreply.github.com>
2023-05-30 00:06:51 +0900

    Update call_seoyeon.py

--- RaspberryPi_src/alert/call_seoyeon.py
+++ RaspberryPi_src/alert/call_seoyeon.py
@@ -1,21 +1,27 @@
 # Code for Emergency Call
 from common.imports import *
 
-# Account info..
-account_sid = 'type dahyun sid' # dahyun account.. will be used
-auth_token = 'type dahyun token'
-twilio_number = '+13159225838'
-recipient_number = '+821090531622' # dahyun phone num for test ,,,,
 
-# Create client
-client = Client(account_sid, auth_token)
+class EmergencyCaller():
+    def __init__(self):
+        self.account_sid = 'type dahyun sid' # dahyun account.. will be used
+        self.auth_token = 'type dahyun token'
+        self.resq4u_number = '+13159225838' # fraud number (just to make calls)
+        self.recipient_number = '+821090531622' # dahyun phone number
+        
+    def callHELP(self):
+        # Create client
+        client = Client(account_sid, auth_token)
 
-# Create call from created client & req. url
-call = client.calls.create(
-    url='http://demo.twilio.com/docs/voice.xml',
-    to=recipient_number,
-    from_=twilio_number
-)
-
-# After making call, print SID log in terminal
-print(call.sid)
+        # Create call from created client & req. url
+        call = client.calls.create(
+            url='http://demo.twilio.com/docs/voice.xml',
+            to=recipient_number,
+            from_=twilio_number
+        )
+        print('Calling Dahyun')
+        print('SID Log: 'call.sid)
+        
+if __name__ == '__main__':
+    caller = EmergencyCaller()
+    caller.callHELP()
