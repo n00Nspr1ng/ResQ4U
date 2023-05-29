0821d28aa9278ac0f550aa53888c10e8e648c8ab
Dahyun Diana Nam <94898107+dn0908@users.noreply.github.com>
2023-05-30 00:51:56 +0900

    Create relay_control.py

---
+++ RaspberryPi_src/alert/relay_control.py
@@ -0,0 +1,24 @@
+from common import config
+from common.imports import *
+
+class Relay:
+    def __init__(self, pin):
+        self.pin = pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT)
+
+    def on(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+
+    def off(self):
+        GPIO.output(self.pin, GPIO.LOW)
+
+    def toggle(self):
+        if GPIO.input(self.pin):
+            self.off()
+        else:
+            self.on()
+
+if __name__ == '__main__':
+    relay_SearchLight = Relay(searchlight )
+    relay_Alert = Relay(alert)
