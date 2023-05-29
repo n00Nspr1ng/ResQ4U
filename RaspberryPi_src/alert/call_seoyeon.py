3e2ba144fc1c5727c992756f1d5cb490b34722b1
Dahyun Diana Nam <94898107+dn0908@users.noreply.github.com>
2023-05-29 23:52:37 +0900

    Update call_seoyeon.py

--- RaspberryPi_src/alert/call_seoyeon.py
+++ RaspberryPi_src/alert/call_seoyeon.py
@@ -1,1 +1,20 @@
-# Calling seoyeon
+# Code for Emergency Call
+
+# Account info..
+account_sid = 'ACb9b1f9a5734af366c962dc679fecb4a7' # dahyun account.. will be used
+auth_token = 'b3f435c08897918d7570d5d9586d90ff'
+twilio_number = '+13159225838'
+recipient_number = '+821090531622' # dahyun phone num for test ,,,,
+
+# Create client
+client = Client(account_sid, auth_token)
+
+# Create call from created client & req. url
+call = client.calls.create(
+    url='http://demo.twilio.com/docs/voice.xml',
+    to=recipient_number,
+    from_=twilio_number
+)
+
+# After making call, print SID log in terminal
+print(call.sid)
