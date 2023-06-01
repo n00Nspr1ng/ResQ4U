d040485c0668de8e5a5ba413af982c21a8c63fd2
dn0908 <diana_nam@yonsei.ac.kr>
2023-06-01 23:14:37 +0900

    Added testing main

---
+++ RaspberryPi_src/testing_code.py
@@ -0,0 +1,53 @@
+mport sys
+sys.path.append('/home/roboin/ResQ4U/RaspberryPi_src/common')
+from imports import *
+
+from pan_tilt.pan_tilt import PanTilt
+from serial_communication.arduino_serial import SerialWrapper
+from camera_detection.detect_person import PersonDetector
+from pan_tilt.stepmotor_control import StepMotorController
+# from alert.relay_control import Relay
+
+# from camera_detection.camera_test import PersonDetector
+
+if __name__ == "__main__":
+
+    # arduino = SerialWrapper(device=config.arduino_uno)
+
+    # print("sending flag f")
+    # arduino.send_flag("f")
+    # while(arduino.end_flag == False):
+    #     arduino.check_end_flag()
+    # print("rasp ended")
+
+    # panMotor = StepMotorController(config.pan_motor, gear_ratio=4)
+    # tiltMotor = StepMotorController(config.tilt_motor, gear_ratio=2)
+    
+    # time.sleep(2)
+    # for i in range(60):
+    #     panMotor.move(angle=0.45, ccw_dir=1)
+    #     tiltMotor.move(angle=0.45, ccw_dir=1)
+    # time.sleep(0.2)
+
+    # panMotor.return_to_initial()
+    # tiltMotor.return_to_initial()
+
+    # time.sleep(0.2)
+    # for i in range(15):
+    #    panMotor.move(angle=0.45, ccw_dir=0)
+    #    tiltMotor.move(angle=0.45, ccw_dir=0)
+    # time.sleep(0.2)
+
+    # panMotor.return_to_initial()
+    # tiltMotor.return_to_initial()
+    
+    # time.sleep(1)
+    # panMotor.move(angle=30, dir=1)
+    # time.sleep(0.2)
+    # panMotor.return_to_initial()
+    
+
+    # pan_tilt = PanTilt(config)
+    # pan_tilt.pan_tilt([300, 200])
+
+    print('hello')
