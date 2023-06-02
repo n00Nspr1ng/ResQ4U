fa60439c6de2214fe90dda7b2a19f842af5443d8
Dahyun Diana Nam <94898107+dn0908@users.noreply.github.com>
2023-06-02 12:14:28 +0900

    Changes on main.py

--- RaspberryPi_src/main.py
+++ RaspberryPi_src/main.py
@@ -13,32 +13,44 @@ from alert.relay_control import Relay
 if __name__ == "__main__":
     print('Hello ResQ4U')
 
+    # inits
     pan_tilt = PanTilt(config)
     arduino = SerialWrapper(device=config.arduino_uno)
     detector = PersonDetector(pan_tilt, arduino, show_image=True)
+    
+    # DETECT
     detector.detect()
     
-    # if detector.is_detected:
-    #     arduino.send_flag("d") # detected
-    #     pan_tilt.pan_tilt([detector.xc, detector.yc])
-    #     if pan_tilt.align_flag == True:
-    #         arduino.send_flag("a")
-
-
+    ###################### WHEN DETECTED LAUNCH! #######################
+    # Call for HELP
     # caller.callHELP() -- test this at last (SID authorization shouldnt be on git public)
 
+    # Turn Search Light ON
     searchLight=Relay(pin = config.searchlight)
     searchLight.on()
     print('Searchlight ON ...')
+    
+    # Turn Alert Siren ON
     alertLight = Relay(pin = config.alert)
     alertLight.on()
     print('Alert ON ...')
 
+    # Read Arduino
     while(arduino.end_flag == False):
         arduino.check_end_flag()
         print(arduino.read_line())
     
+    #################### AFTER LAUNHCH RETURN INIT ####################
+    
     print("END ... Returning to initial state ...")
     
+    # PanTilt to initial State
     pan_tilt.return_to_init()
     
+    # Turn Search Light OFF
+    searchLight.off()
+    print('Searchlight OFF ...')
+
+    # Turn Alert Siren OFF
+    alertLight.off()
+    print('Alert OFF ...')
